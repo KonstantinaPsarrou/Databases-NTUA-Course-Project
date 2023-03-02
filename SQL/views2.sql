8a93002e37e921549ff705caea3e001ffeb17f90
KonstantinaPsarrou <81778154+KonstantinaPsarrou@users.noreply.github.com>
2023-03-02 18:58:53 +0200

    Update views2.sql

--- SQL/views2.sql
+++ SQL/views2.sql
@@ -2,4 +2,3 @@ create view Customer_info as
   select Customer.NFC_ID, FName, LName, BirthDate, DocNumber, DocType, Issuer, Email, Phone
   from Customer, Phone, Email
   where (Customer.NFC_ID = Phone.NFC_ID and Customer.NFC_ID = Email.NFC_ID)
- 
